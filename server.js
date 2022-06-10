ba8b9ae6e5eac24ff9ea25ce505fd268d5cc78dd
lotus <daniel.younghwan.lee@gmail.com>
2022-06-10 19:14:13 -0400

    link front-end to back-end

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const path = require('path');
 
 const { animals } = require('./data/animals');
 const express = require('express');
+const { addAbortSignal } = require('stream');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -96,6 +99,22 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
